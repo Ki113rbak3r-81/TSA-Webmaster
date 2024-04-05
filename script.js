26c8a21de45d6d46ee32f3abe927cf0cac03e25b
Ki113rbak3r-81 <117477972+Ki113rbak3r-81@users.noreply.github.com>
2024-04-05 22:13:22 +0000

    triagram for heaven almost done, need to fix nav

---
+++ script.js
@@ -0,0 +1,19 @@
+const initApp = () => {
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        hamburgerBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
+
+
+
