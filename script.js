ef26a31f95f0c3ded2ec8aa58fa9379283835b32
Ki113rbak3r-81 <117477972+Ki113rbak3r-81@users.noreply.github.com>
2024-04-11 03:12:13 +0000

    Stuff

--- script.js
+++ script.js
@@ -39,17 +39,17 @@ if (window.location.href.includes("energy-template.html")) {
     }
 
     const solar = new energy("Solar", 
-        "<h3>Green Energy At Home</h3><p>You can use solar power to both provide electricity and heating to your home.</p>",
+        "<h3>Solar Energy At Home</h3><p>You can use solar power to both provide electricity and heating to your home.</p><img src=\"solar.jpg\">",
         "<h3>Basic Info</h3><p>Two Types of solar electric systems</p><ul><li>Grid tied - home generates own electricity, but can use the power grid at night if needed</li><li>Off grid - not connected to the grid, is self sufficient, stores electricity in batteries for the night</li></ul>",
         "<h3>Tax Rebates</h3><h4>Oregon Solar + Storage Rebate</h4><ul><li>Is for homeowners that want to install solar electric systems and storage.</li><li>Normal income homeowners can receive up to $5000 for the solar electric system and up to $2500 for the storage systems</li><li>Low income providers cna save up to $30000 for solar, and $15000 for storage.<ul><li>Must be installed by an ODOE approved contractor + both systems must be bought together in order to get the paired storage rebate.</li></ul></li></ul>",
         "<h3>Costs</h3><ul><li>Grid tied system -- approximately $15,000 - $20,000</li><li>Off grid system -- approximately $45,000 - $65,000</li><li>Pay themselves off after approximately 8 years, but can range anywhere from 5 to 15.</li></ul>"
     )
 
     const geo = new energy("Geothermal",
-        "Fortnite",
-        "<p>Fortnite</p>",
-        "<p>Fortnite</p>",
-        "<p>and fortnite</p>"
+        "",
+        "",
+        "",
+        ""
     )
 
     const bio = new energy("Biomass",
